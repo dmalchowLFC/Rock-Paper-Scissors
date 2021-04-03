2c3411d5e0f98d6af7fa2b59143e63a40c48c9f1
dmalchowLFC <80545257+dmalchowLFC@users.noreply.github.com>
2021-04-03 14:54:57 -0700

    Changed up the appearance a bit
    Just moved the Computer's choice text underneath the hands and then added some additional text to make it a little clearer about what's happening while playing the game.

--- rps.js
+++ rps.js
@@ -37,41 +37,41 @@ function pickScissors() {
 function winLoseDraw(userChoice, compChoice) {
     if (userChoice === 0) {
         if (compChoice === 0) {
-            compMove.textContent = "Rock!"
+            compMove.textContent = "Computer chose Rock!"
             result.textContent = "It's a draw!";
         } else if (compChoice === 1) {
             ++compScore
-            compMove.textContent = "Paper!";
+            compMove.textContent = "Computer chose Paper!";
             result.textContent = "You lose!";
         } else if (compChoice === 2) {
             ++userScore
-            compMove.textContent = "Scissors!";
+            compMove.textContent = "Computer chose Scissors!";
             result.textContent = "You win!";
         }
     } else if (userChoice === 1) {
         if (compChoice === 0) {
             ++userScore
-            compMove.textContent = "Rock!";
+            compMove.textContent = "Computer chose Rock!";
             result.textContent = "You win!";
         } else if (compChoice === 1) {
-            compMove.textContent = "Paper!";
+            compMove.textContent = "Computer chose Paper!";
             result.textContent = "It's a draw!";
         } else if (compChoice === 2) {
             ++compScore
-            compMove.textContent = "Scissors!";
+            compMove.textContent = "Computer chose Scissors!";
             result.textContent = "You lose!";
         }
     } else if (userChoice === 2) {
         if (compChoice === 0) {
             ++compScore;
-            compMove.textContent = "Rock!";
+            compMove.textContent = "Computer chose Rock!";
             result.textContent = "You lose!";
         } else if (compChoice === 1) {
             ++userScore;
-            compMove.textContent = "Paper!"
+            compMove.textContent = "Computer chose Paper!"
             result.textContent = "You win";
         } else if (compChoice === 2) {
-            compMove.textContent = "Scissors!"
+            compMove.textContent = "Computer chose Scissors!"
             result.textContent = "It's a draw!";
         }
     }
